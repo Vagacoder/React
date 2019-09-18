6b2f5e05c94f9bbd4fed181dc42b430b2b192018
Vagacoder <qiruihu@gmail.com>
2019-09-17 23:29:21 -0700

    update 31 full calendar

--- 31-full-calendar-test1/src/App.tsx
+++ 31-full-calendar-test1/src/App.tsx
@@ -16,20 +16,14 @@ import './calendar.css';
 import './dayGrid.css';
 import './timeGrid.css';
 import './list.css';
+import { EventApi, View } from '@fullcalendar/core';
 
 const dummy_events = [
   {
     title: 'event 1',
-    date: '2019-09-17 10:15'
-  },
-  {
-    title: 'event 3',
-    date: '2019-09-17 17:05'
-  },
-  {
-    title: 'event 4',
-    start: '2019-09-17 14:30',
-    end: '2019-09-17 19:30'
+    date: '2019-09-17 10:15',
+    backgroundColor: 'rgba(129, 199, 132, 0.8)',
+    borderColor: 'red'
   },
   {
     title: 'event 2',
@@ -37,9 +31,31 @@ const dummy_events = [
     allDay: true
   },
   {
+    title: 'event 3',
+    date: '2019-09-17 17:05',
+    extendedProps: {
+      status: 'LEAD'
+    }
+  },
+  {
+    title: 'event 4',
+    start: '2019-09-17 14:30',
+    end: '2019-09-17 19:30',
+    extendedProps: {
+      status: 'BOOKED'
+    }
+  },
+  {
     title: 'event 5',
     start: '2019-09-17 13:15',
-    allDay: true
+    allDay: true,
+  },
+  {
+    title: 'event 6',
+    start: '2019-09-19 15:45',
+    end: '2019-09-19 18:30',
+    backgroundColor: 'rgba(229, 115, 115, 0.8)',
+    textColor: 'gold' 
   }
 ];
 
@@ -55,7 +71,7 @@ const timeZoneString = 'America/Los_Angeles';
 const timeFormatString = 'MMMM DD YYYY';
 
 const headers = {
-  left: 'dayGridMonth,timeGridWeek,timeGridDay, custom1',
+  left: 'dayGridMonth,timeGridWeek,timeGridDay, listWeek,listMonth, custom1',
   center: 'title',
   right: 'custom2, prevYear,prev,today,next,nextYear'
 };
@@ -75,7 +91,27 @@ const customButtons = {
     text: 'custom 2',
     click: () => { alert('custom 2 is clicked!'); }
   }
-}
+};
+
+const eventRender = (info: {
+  isMirror: boolean;
+  isStart: boolean;
+  isEnd: boolean;
+  event: EventApi;
+  el: HTMLElement;
+  view: View;
+}) => {
+  if(info.event.extendedProps.status === "LEAD"){
+    info.el.style.backgroundColor = 'rgba(229, 115, 115, 0.8)';
+    info.el.style.color = 'gold';
+    var dotEl : any = info.el.getElementsByClassName('fc-event-dot')[0];
+      if (dotEl) {
+        dotEl.style.backgroundColor = 'white';
+      }
+  } else if ((info.event.extendedProps.status === "BOOKED")) {
+    info.el.style.backgroundColor = 'rgba(129, 199, 132, 0.8)';
+  }
+};
 
 function App() {
   return (
@@ -83,8 +119,9 @@ function App() {
       <FullCalendar
         aspectRatio={1.5}
         fixedWeekCount={false}
-        allDayText="All Day"
+        allDayText="All Day Event"
         slotDuration='00:30:00'
+        nowIndicator={true}
         header={headers}
         footer={footers}
         customButtons={customButtons}
@@ -99,8 +136,13 @@ function App() {
             bootstrapPlugin
           ]}
         events={dummy_events}
+        eventRender={eventRender}
         titleFormat={timeFormatString}
         timeZone={timeZoneString}
+        views={{
+          listWeek: { buttonText: 'Week list' },
+          listMonth: { buttonText: 'Month list' }
+        }}
       //themeSystem="bootstrap"
       />
     </div>
