07086daef15e5b1a298f11ce0e57bd221c261b30
Vagacoder <qiruihu@gmail.com>
2019-10-03 08:34:14 -0700

    uodate material select

--- 32-material-select1/src/Select1.tsx
+++ 32-material-select1/src/Select1.tsx
@@ -5,11 +5,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import { Typography } from '@material-ui/core';
 
 function Select1() {
 
   const [values, setValues] = useState({
-    age: '',
+    age: 5,
     name: 'hai',
   });
 
@@ -38,12 +39,13 @@ function Select1() {
         id:'age-simple',
       }}
       >
+        <MenuItem value={5}> Five </MenuItem>
         <MenuItem value={10}>  Ten    </MenuItem>
         <MenuItem value={20}>  Twenty    </MenuItem>
         <MenuItem value={30}>  Thitry    </MenuItem>
       </Select>
 
-
+      <Typography>{`Age: ${values.age}`}</Typography>
     </div>
   );
 }
