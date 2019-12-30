510df428b952c6a020a7351eb0a455f69f6c57c2
Vagacoder <qiruihu@gmail.com>
2019-12-30 11:29:59 -0800

    update 47 storybook example 1

---
+++ 47-storybook1/src/stories/App.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from '../App';
+
+export default { title: 'React default App' };
+
+export const app = () => {
+    return <App />;
+};
+
+app.story = { name: "React App Default Page", };
