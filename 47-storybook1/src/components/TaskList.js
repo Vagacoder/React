d50f3f83eb04bd1a1e9bf877eae618c2d747d400
Vagacoder <qiruihu@gmail.com>
2019-12-30 13:21:27 -0800

    update storybook example to 'screens'

--- 47-storybook1/src/components/TaskList.js
+++ 47-storybook1/src/components/TaskList.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Task from './Task';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
 
 const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
     const events = {
@@ -18,10 +21,57 @@ const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
     return (
         <div className="list-items">
             {tasks.map((task) => {
-                <Task key={task.id} task={task} {...events} />
+                return <Task key={task.id} task={task} {...events} />
             })}
         </div>
     );
 }
 
-export default TaskList;
+export const PureTaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
+    const events = {
+        onPinTask,
+        onArchiveTask,
+    };
+
+    if (loading) {
+        return <div className="list-items">loading</div>;
+    }
+
+    if (tasks.length === 0) {
+        return <div className="list-items">empty</div>;
+    }
+
+    return (
+        <div className="list-items">
+            {tasks.map((task) => {
+                return <Task key={task.id} task={task} {...events} />
+            })}
+        </div>
+    );
+}
+
+PureTaskList.propTypes = {
+    loading: PropTypes.bool,
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+    onPinTask: PropTypes.func.isRequired,
+    onArchiveTask: PropTypes.func.isRequired,
+};
+
+PureTaskList.defaultProps = {
+    loading: false,
+};
+
+// export default TaskList;
+
+export default connect(
+    ({ tasks }) => ({
+        tasks: tasks.filter((t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+    }),
+    dispatch => ({
+        onArchiveTask: id => dispatch(archiveTask(id)),
+        onPinTask: id => dispatch(pinTask(id)),
+    })
+)(PureTaskList);
+
+
+
